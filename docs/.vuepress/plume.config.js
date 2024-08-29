a16fa01c3905a921016c744058eb1b58a6cd8f4d
chgoh7 <3180349973@qq.com>
2024-08-29 09:04:54 +0800

    docs: alter giscus theme

--- docs/.vuepress/plume.config.js
+++ docs/.vuepress/plume.config.js
@@ -2,7 +2,7 @@
  * @Author: chgoh7 3180349973@qq.com
  * @Date: 2024-08-26 16:44:40
  * @LastEditors: chgoh7 3180349973@qq.com
- * @LastEditTime: 2024-08-29 08:57:43
+ * @LastEditTime: 2024-08-29 09:04:29
  * @FilePath: \it-docs\docs\.vuepress\plume.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -57,8 +57,8 @@ export default defineThemeConfig({
             repoId: 'R_kgDOMpwR7A',
             category: 'General',
             categoryId: 'DIC_kwDOMpwR7M4CiBWo',
-            darkTheme: 'GitHub Dark',
-            lightTheme: 'GitHub Light'
+            darkTheme: 'dark',
+            lightTheme: 'light'
         }
     }
 })
