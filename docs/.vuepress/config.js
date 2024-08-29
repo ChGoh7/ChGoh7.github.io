2de18e9421c3cbf960312723b8b9b6822223fc58
chgoh7 <3180349973@qq.com>
2024-08-29 12:15:39 +0800

    docs: alter default comment

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@
  * @Author: chgoh7 3180349973@qq.com
  * @Date: 2024-08-26 16:40:39
  * @LastEditors: chgoh7 3180349973@qq.com
- * @LastEditTime: 2024-08-29 11:58:13
+ * @LastEditTime: 2024-08-29 12:15:15
  * @FilePath: \it-docs\docs\.vuepress\config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -20,7 +20,7 @@ export default defineUserConfig({
         plugins: {
             comment: {
                 provider: 'Giscus', // "Artalk“ | "Giscus" | "Twikoo" | "Waline"
-                comment: true,
+                comment: false,
                 repo: 'ChGoh7/ChGoh7.github.io',
                 repoId: 'R_kgDOMoXr5A',
                 category: 'General',
