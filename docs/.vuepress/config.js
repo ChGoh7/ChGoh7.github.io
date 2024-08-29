a06e9fc79e6467a773d814ed29c52537669a89b6
chgoh7 <3180349973@qq.com>
2024-08-29 11:58:46 +0800

    docs: add comment

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,14 +2,13 @@
  * @Author: chgoh7 3180349973@qq.com
  * @Date: 2024-08-26 16:40:39
  * @LastEditors: chgoh7 3180349973@qq.com
- * @LastEditTime: 2024-08-29 11:43:44
+ * @LastEditTime: 2024-08-29 11:58:13
  * @FilePath: \it-docs\docs\.vuepress\config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import { defineUserConfig } from 'vuepress/cli'
 import { viteBundler } from '@vuepress/bundler-vite'
 import { plumeTheme } from 'vuepress-theme-plume'
-import { commentPlugin } from 'vuepress-theme-plume'
 // import { webpackBundler } from '@vuepress/bundler-webpack'
 
 export default defineUserConfig({
