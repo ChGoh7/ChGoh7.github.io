012d381a9cbebd2a5059939b7b749dd09a7e69a3
chgoh7 <3180349973@qq.com>
2024-09-06 16:40:58 +0800

    docs: add notes

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@
  * @Author: chgoh7 3180349973@qq.com
  * @Date: 2024-08-26 16:40:39
  * @LastEditors: chgoh7 3180349973@qq.com
- * @LastEditTime: 2024-09-03 15:55:27
+ * @LastEditTime: 2024-09-06 16:13:58
  * @FilePath: \it-docs\docs\.vuepress\config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -39,8 +39,43 @@ export default defineUserConfig({
                 appId: '6UHIQ8YOAT',
                 apiKey: 'ba57eb4f0385f95ebe4e4287fd4d4e27',
                 indexName: 'chgoh7',
-            }
+            }, readingTime: {
+                wordPerMinute: 300
+            }, markdownEnhance: {
+                hint: true, // 提示容器
+                codetabs: true, // 代码组
+                tabs: true, // 选项卡
+                align: true, // 对齐容器
+                mark: true, // 标记语法
+                tasklist: true, // 任务列表语法
+                attrs: true, // 属性语法
+                sup: true, // 上标语法
+                sub: true, // 下标语法
+                alert: true, // GFM 通知语法
+                footnote: true, // 注脚语法
+                katex: true, // 数学公式
 
+                // 以下可选项在 主题中默认不启用，
+                // 请在主题中自行配置
+                // include: true, // Markdown 导入支持
+                // figure: true, // 启用图片 Figure 支持
+                // imgLazyload: true, // 使用原生方式懒加载页面图片
+                // imgMark: true, // 浅色/深色 图片标记
+                // imgSize: true, // 图片尺寸支持
+                // obsidianImgSize: true, // obsidian 图片尺寸支持
+                // mathjax: true, //  Math Jax 数学公式 语法支持
+                // chart: true, // 图表支持
+                // echarts: true, // ECharts 图表支持
+                // flowchart: true, // 流程图支持
+                // markmap: true, // Markmap 图表支持
+                // stylize: true, // 对行内语法进行样式化以创建代码片段
+                // playground: true, // 交互演示
+                // kotlinPlayground: true, // Kotlin 交互演示
+                // vuePlayground: true, // Vue 交互演示
+                // sandpack: true, // sandpack 交互演示
+                // demo: true, // 代码案例
+                // revealJs: true, // 幻灯片支持
+            }
         }
     }),
     bundler: viteBundler()
---
+++ docs/notes/JavaEE/05 Spring/index.md
@@ -0,0 +1,18 @@
+> 查看[springframework文档](https://docs.spring.io/spring/docs/current/spring-framework-reference/index.html)
+
+* 对于 Spring 框架而言，一切 Java 对象都是 Bean
+* Spring 的 IoC 容器降低了业务对象替换的复杂性，提高了组件之间的解耦
+* Spring 的 AOP 支持允许将一些通用任务如安全、事务、日志等进行集中式处理，从而提供了更好的复用
+* Spring 的 ORM 和 DAO 提供了与第三方持久层框架的良好整合，并简化了底层的数据库访问
+* 注意：Spring 的核心容器必须依赖于 common-logging.jar
+
+
+
+<!-- slide -->
+
+Hi here
+
+<!-- slide -->
+let us go
+<!-- slide -->
+Thank you!
