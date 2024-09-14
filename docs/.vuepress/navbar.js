e7dab0378c2de5f4164938bfa8b1b449da0ffc63
chgoh7 <3180349973@qq.com>
2024-09-14 18:20:06 +0800

    docs: build process

--- docs/.vuepress/navbar.js
+++ docs/.vuepress/navbar.js
@@ -2,7 +2,7 @@
  * @Author: chgoh7 3180349973@qq.com
  * @Date: 2024-08-26 16:47:03
  * @LastEditors: chgoh7 3180349973@qq.com
- * @LastEditTime: 2024-09-13 17:50:12
+ * @LastEditTime: 2024-09-14 18:19:41
  * @FilePath: \it-docs\docs\.vuepress\navbar.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
